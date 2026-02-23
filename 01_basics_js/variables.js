6dc87b97e9898369beeb5a69d5336ba721626c91
avijitbose-prog <avijit.bose@keross.com>
2026-02-23 07:05:05 +0000

    varibles

---
+++ 01_basics_js/variables.js
@@ -0,0 +1,16 @@
+const accountId=1234;
+//const means constanat means it cant be change
+let accountEmail="avijit@gmail.com";
+var accountPass="12345";
+accountCity="kolkata";
+let accountstate;
+console.log(accountId);
+// console.table([accountEmail,accountId,accountstate,accountpass,accountcity]);
+accountEmail = "hc@hc.com";
+accountPass = "21212121";
+accountCity = "Bengaluru";
+console.table([accountEmail,accountId,accountstate,accountPass,accountCity]);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/

d96d74f42a49ba47d4d605664d01fd57ddca1b5c
avijitbose-prog <avijit.bose@keross.com>
2026-02-23 12:45:37 +0000

    conversion

---
+++ conversions.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+//comparison converts null to 0
+
+// === 
+//this === means strict checking more correctly the data type should be same
+console.log("2" === 2);

556b78c6fc25e3a1ca7fa2aa6d114547d2023ba4
avijitbose-prog <avijit.bose@keross.com>
2026-02-23 10:44:40 +0000

    conversion

---
+++ 01_basics_js/conversions.js
@@ -0,0 +1,21 @@
+let score="hitesh";
+//console.log(typeof score);
+//console.log(typeof(score));
+let valueinnum=Number(score);//show nan means not a number if the input is string
+//console.log(typeof valueinnum);
+//console.log(valueinnum);
+//"33"->will show 33 in integer after conversion
+//"33abs"->show NaN(not a number) because a string is present there
+//true->1 and false->0
+let isLoggedIn="avijit";
+let boolisloggedin=Boolean(isLoggedIn);
+console.log(boolisloggedin);
+// 1 => true; 0 => false
+// "" => false
+// "avijit" => true
+
+let somenumber=23;
+let stringsomenumber=String(somenumber);
+console.log(stringsomenumber);
+console.log(typeof stringsomenumber);
+
